6a829b3d130787215a45071ce26c7bfcae892e6b
Done83 <nedomehan@gmail.com>
2023-10-31 10:41:50 +0100

    Reset button added and input filed changed

--- script.js
+++ script.js
@@ -1,19 +1,21 @@
 const convertButton = document.getElementById('convertButton');
+const resetButton = document.getElementById('resetButton');
+const inputText = document.getElementById('inputTextField'); // Define inputText here
 const outputText = document.getElementById('outputText');
 
 const modifyText = () => {
-    const inputText = document.getElementById('inputTextField').value;
-    const words = inputText.split(' ');
+    const inputTextValue = inputText.value;
+    const words = inputTextValue.split(' ');
     let modifiedText = '';
 
     words.forEach((word) => {
         let modifiedWord = '';
         if (word.length >= 7) {
             modifiedWord = `<b>${word.substring(0, 3)}</b>${word.substring(3)}`;
-            console.log(modifiedWord)
+            console.log(modifiedWord);
         } else {
             modifiedWord = `<b>${word.substring(0, 2)}</b>${word.substring(2)}`;
-            console.log(modifiedWord)
+            console.log(modifiedWord);
         }
         modifiedText += modifiedWord + ' ';
     });
@@ -23,7 +25,14 @@ const modifyText = () => {
 
 convertButton.addEventListener('click', () => {
     modifyText();
-})
+});
+
+resetButton.addEventListener('click', () => {
+    inputText.value = ''; // Now you can reset the input field
+    outputText.innerHTML = '';
+});
+
+
 
 
 
